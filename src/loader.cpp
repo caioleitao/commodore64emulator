4a7e06e4a9138503a2b96dc13017e164e6b96533
Caio Cesar <kylespace@icloud.com>
2021-07-19 01:38:14 -0300

    add main.cpp

--- src/loader.cpp
+++ src/loader.cpp
@@ -2,3 +2,86 @@
 // Created by Kyle on 7/14/21.
 //
 
+#include "loader.h"
+
+Loader::Loader(C64 *c64) {
+    c64_ = c64;
+    io_  = c64_->io();
+    cpu_ = c64_->cpu();
+    mem_ = c64_->memory();
+    booted_up_ = false;
+    format_ = kNone;
+}
+
+uint16_t Loader::read_short_le() {
+    char b;
+    uint16_t v = 0;
+    is_.get(b);
+    v |= (b);
+    is_.get(b);
+    v |= b << 8;
+    return v;
+}
+
+void Loader::bas(const std::string &f) {
+    format_ = kBasic;
+    is_.open(f,std::ios::in);
+}
+
+void Loader::load_basic() {
+    char c;
+    if(is_.is_open()) {
+        while(is_.good()) {
+            is_.get(c);
+            io_->IO::type_character(c);
+        }
+    }
+}
+
+void Loader::prg(const std::string &f) {
+    format_ = kPRG;
+    is_.open(f,std::ios::in|std::ios::binary);
+}
+
+void Loader::load_prg() {
+    char b;
+    uint16_t pbuf, addr;
+    pbuf = addr = read_short_le();
+    if(is_.is_open()) {
+        while(is_.good()) {
+            is_.get(b);
+            mem_->write_byte_no_io(pbuf++,b);
+        }
+        if(addr == kBasicPrgStart) {
+            mem_->write_word_no_io(kBasicTxtTab,kBasicPrgStart);
+            mem_->write_word_no_io(kBasicVarTab,pbuf);
+            mem_->write_word_no_io(kBasicAryTab,pbuf);
+            mem_->write_word_no_io(kBasicStrEnd,pbuf);
+
+            for(char &c: std::string("RUN\n"))
+                io_->IO::type_character(c);
+        }
+        else cpu_->pc(addr);
+    }
+}
+
+bool Loader::emulate() {
+    if(booted_up_) {
+        switch (format_) {
+            case kBasic:
+                load_basic();
+                break;
+            case kPRG:
+                load_prg();
+                break;
+            default:
+                break;
+        }
+        return false;
+    }
+    else {
+        if(cpu_->pc() == 0xa65c)
+            booted_up_ = true;
+    }
+    return true;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,3 +2,76 @@
 // Created by Kyle on 7/14/21.
 //
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+#include "c64.h"
+#include "loader.h"
+#ifdef EMSCRIPTEN
+#include <emscripten.h>
+#endif
+
+C64 *c64;
+Loader *loader;
+bool wget_download_finished = false;
+
+bool loader_cb() {
+    if(!loader->emulate())
+        c64->callback(nullptr);
+    return true;
+}
+
+bool emscripten_loader_cb() {
+    if (!loader->emulate() && wget_download_finished)
+        c64->callback(nullptr);
+    return true;
+}
+
+void load_file(const char *file) {
+    std::string f(file);
+    size_t ext_i = f.find_last_of(" . ");
+    if(ext_i != std::string::npos) {
+        std::string ext(f.substr(ext_i+1));
+        std::transform(ext.begin(),ext.end(),ext.begin(),::tolower);
+        if(ext == "bas"){
+            loader->bas(f);
+        } else if(ext == "prg"){
+            loader->prg(f);
+        }
+    }
+}
+
+void wget_cb(const char *f) {
+    wget_download_finished = true;
+    load_file(f);
+}
+
+void emscripten_loop() {
+    c64->emscripten_loop();
+}
+
+int main(int argc, char **argv) {
+    c64 = new C64();
+    if(argc != 1) {
+        loader = new Loader(c64);
+#ifdef EMSCRIPTEN
+        std::string f(argv[1]);
+        size_t sp = f.find_last_of("/");
+        if(sp != std::string::npos) {
+            std::string fname(f.substr(sp+1));
+            c64->callback(emscripten_loader_cb);
+            emscripten_async_wget(argv[1],fname.c_str(),wget_cb,nullptr);
+        }
+#else
+        c64->callback(loader_cb);
+        load_file(argv[1]);
+#endif
+    }
+#ifdef EMSCRIPTEN
+    emscripten_set_main_loop(emscripten_loop,0,0);
+#else
+    c64->start();
+#endif
+    return 0;
+}
